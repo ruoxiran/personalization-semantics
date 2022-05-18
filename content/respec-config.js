79ac8d160b62920ccce0bada9f23befc3062ec10
ruoxiran <ran@w3.org>
2022-05-18 13:22:58 +0800

    Update respec-config.js

--- content/respec-config.js
+++ content/respec-config.js
@@ -103,22 +103,25 @@ var respecConfig = {
     // errata: 'http://www.w3.org/2010/02/rdfa/errata.html',
 
     // name of the WG
-    wg:           "Accessible Platform Architectures Working Group",
+    //wg:           "Accessible Platform Architectures Working Group",
 
     // URI of the public WG page
-    wgURI:        "https://www.w3.org/WAI/APA/",
+    //wgURI:        "https://www.w3.org/WAI/APA/",
 
     // name (with the @w3c.org) of the public mailing to which comments are due
-    wgPublicList: "public-personalization-tf",
+    //wgPublicList: "public-personalization-tf",
 
     // URI of the patent status for this WG, for Rec-track documents
     // !!!! IMPORTANT !!!!
     // This is important for Rec-track documents, do not copy a patent URI from a random
     // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
     // Team Contact.
-    wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/83907/status",
+    //wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/83907/status",
     //maxTocLevel: 2,
 
-    localBiblio: biblio,
+    //localBiblio: biblio,
+    group: "apa",
+	github: "w3c/naur",
+	maxTocLevel: 4
 
   };
